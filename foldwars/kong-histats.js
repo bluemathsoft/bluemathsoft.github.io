a44ea18187ae7e03334d5706993a5e51d56e238a
Jayesh Salvi <jayeshsalvi@gmail.com>
2018-04-25 11:59:45 -0400

    Foldwars histats update

--- foldwars/kong-histats.js
+++ foldwars/kong-histats.js
@@ -1,205 +1,265 @@
 try{window.FOLDWARS_KONG_HISTATS={
-  "ebl_cairo_hard": {
-    "value": 118,
-    "player": "ImperialUser"
-  },
   "ebl_cairo_normal": {
     "value": 0
   },
-  "ebl_corinth_hard": {
-    "value": 254,
-    "player": "ballzac"
+  "ebl_cairo_hard": {
+    "value": 118,
+    "player": "ImperialUser"
   },
   "ebl_florentia_hard": {
     "value": 160,
     "player": "DotaCota"
   },
-  "ebl_babylon_hard": {
-    "value": 122,
+  "ebl_corinth_hard": {
+    "value": 254,
     "player": "ballzac"
   },
   "ebl_rome_hard": {
     "value": 328,
     "player": "ballzac"
   },
-  "ebl_memphis_hard": {
-    "value": 150,
-    "player": "Tryko"
+  "ebl_cairo_easy": {
+    "value": 178,
+    "player": "mideg"
+  },
+  "kps_babylon_easy": {
+    "value": 0
+  },
+  "kps_babylon_hard": {
+    "value": 7,
+    "player": "Electricojsurfr"
+  },
+  "kps_corinth_hard": {
+    "value": 14,
+    "player": "daddio2"
+  },
+  "ebl_florentia_easy": {
+    "value": 172,
+    "player": "DavidNoah"
+  },
+  "ebl_corinth_normal": {
+    "value": 268,
+    "player": "lineover"
+  },
+  "ebl_babylon_hard": {
+    "value": 122,
+    "player": "ballzac"
+  },
+  "kps_cairo_hard": {
+    "value": 6,
+    "player": "ImperialUser"
   },
   "ebl_babylon_normal": {
     "value": 132,
     "player": "atexcullen"
   },
-  "ebl_cairo_easy": {
-    "value": 178,
-    "player": "mideg"
-  },
-  "ebl_thebes_hard": {
-    "value": 138,
-    "player": "ImperialUser"
-  },
-  "ebl_memphis_normal": {
+  "ebl_memphis_easy": {
     "value": 154,
+    "player": "vgfvgf"
+  },
+  "ebl_mithila_easy": {
+    "value": 164,
+    "player": "Ambellina"
+  },
+  "ebl_memphis_hard": {
+    "value": 150,
     "player": "Tryko"
   },
   "ebl_babylon_easy": {
     "value": 146,
     "player": "Satan206"
   },
+  "ebl_mithila_hard": {
+    "value": 148,
+    "player": "Oninou"
+  },
+  "ebl_memphis_normal": {
+    "value": 154,
+    "player": "Tryko"
+  },
   "ebl_florentia_normal": {
     "value": 162,
     "player": "QuantumT"
   },
-  "ebl_rome_easy": {
-    "value": 558,
-    "player": "tesco1"
-  },
-  "kps_babylon_easy": {
-    "value": 0
-  },
-  "ebl_thebes_normal": {
-    "value": 150,
-    "player": "Moog94"
+  "ebl_thebes_hard": {
+    "value": 138,
+    "player": "ImperialUser"
   },
   "ebl_corinth_easy": {
     "value": 520,
     "player": "tesco1"
   },
-  "ebl_memphis_easy": {
-    "value": 154,
-    "player": "KakkoiiBishounen"
+  "kps_memphis_easy": {
+    "value": 10,
+    "player": "Sytse75"
   },
-  "ebl_mithila_easy": {
-    "value": 164,
-    "player": "atexcullen"
-  },
-  "ebl_corinth_normal": {
-    "value": 268,
-    "player": "lineover"
-  },
-  "ebl_mithila_hard": {
-    "value": 148,
-    "player": "Oninou"
-  },
-  "ebl_florentia_easy": {
-    "value": 172,
-    "player": "DavidNoah"
+  "ebl_thebes_normal": {
+    "value": 150,
+    "player": "Moog94"
   },
   "ebl_mithila_normal": {
     "value": 164,
     "player": "cdgx"
   },
-  "kps_cairo_hard": {
-    "value": 6,
-    "player": "ImperialUser"
-  },
-  "kps_corinth_easy": {
-    "value": 17,
-    "player": "Elpowermoe"
-  },
-  "kps_corinth_normal": {
-    "value": 15,
-    "player": "JuggernautBlue"
-  },
-  "kps_babylon_hard": {
-    "value": 7,
-    "player": "Electricojsurfr"
-  },
-  "ebl_thebes_easy": {
-    "value": 158,
-    "player": "Elpowermoe"
-  },
-  "ebl_rome_normal": {
-    "value": 348,
-    "player": "Moog94"
+  "kps_florentia_hard": {
+    "value": 10,
+    "player": "ChiefStorm"
   },
   "kps_rome_hard": {
     "value": 13,
     "player": "ballzac"
   },
-  "kps_florentia_hard": {
-    "value": 10,
-    "player": "ChiefStorm"
-  },
   "kps_cairo_easy": {
     "value": 11,
     "player": "Azelie"
-  },
-  "kps_memphis_easy": {
-    "value": 10,
-    "player": "Sytse75"
   },
   "kps_babylon_normal": {
     "value": 8,
     "player": "JJL24SS"
   },
-  "score_corinth_hard": {
-    "value": 183858,
+  "ebl_thebes_easy": {
+    "value": 158,
+    "player": "Elpowermoe"
+  },
+  "ebl_rome_easy": {
+    "value": 558,
+    "player": "tesco1"
+  },
+  "ebl_rome_normal": {
+    "value": 348,
+    "player": "Moog94"
+  },
+  "kps_corinth_normal": {
+    "value": 15,
+    "player": "JuggernautBlue"
+  },
+  "kps_cairo_normal": {
+    "value": 8,
+    "player": "deathbyanangel66"
+  },
+  "kps_mithila_hard": {
+    "value": 9,
+    "player": "Oninou"
+  },
+  "kps_corinth_easy": {
+    "value": 17,
+    "player": "Elpowermoe"
+  },
+  "kps_mithila_normal": {
+    "value": 10,
+    "player": "bittramp"
+  },
+  "score_rome_hard": {
+    "value": 525288,
     "player": "ballzac"
   },
-  "kps_florentia_normal": {
-    "value": 9,
-    "player": "christobuu"
-  },
-  "score_cairo_hard": {
-    "value": 72576,
-    "player": "ImperialUser"
-  },
-  "kps_corinth_hard": {
-    "value": 14,
+  "kps_memphis_hard": {
+    "value": 11,
     "player": "daddio2"
   },
   "kps_florentia_easy": {
     "value": 8,
     "player": "Gosick87"
   },
-  "score_mithila_hard": {
-    "value": 72240,
-    "player": "Martyr224"
+  "kps_mithila_easy": {
+    "value": 12,
+    "player": "tesco1"
   },
-  "score_rome_hard": {
-    "value": 525288,
-    "player": "ballzac"
+  "score_thebes_normal": {
+    "value": 121290,
+    "player": "Ticwith"
   },
-  "score_corinth_easy": {
-    "value": 185240,
-    "player": "samus2679"
+  "score_memphis_easy": {
+    "value": 53568,
+    "player": "mrbb62964"
   },
-  "score_babylon_hard": {
-    "value": 69494,
-    "player": "ballzac"
+  "kps_memphis_normal": {
+    "value": 10,
+    "player": "Znarf"
   },
-  "score_memphis_normal": {
-    "value": 63320,
-    "player": "blumath"
-  },
-  "kps_cairo_normal": {
-    "value": 8,
-    "player": "Mezzanotte666"
-  },
-  "kps_mithila_hard": {
+  "kps_florentia_normal": {
     "value": 9,
-    "player": "Oninou"
+    "player": "christobuu"
+  },
+  "score_babylon_easy": {
+    "value": 81400,
+    "player": "Leg0Lars"
+  },
+  "kps_rome_easy": {
+    "value": 11,
+    "player": "zentaa"
   },
   "kps_thebes_hard": {
     "value": 9,
     "player": "Martyr224"
   },
-  "kps_memphis_hard": {
-    "value": 11,
-    "player": "daddio2"
+  "score_corinth_easy": {
+    "value": 185240,
+    "player": "samus2679"
   },
-  "score_cairo_easy": {
-    "value": 122672,
-    "player": "Hashyuba"
+  "score_babylon_normal": {
+    "value": 77630,
+    "player": "olinn"
   },
-  "kps_mithila_easy": {
-    "value": 12,
-    "player": "tesco1"
+  "score_mithila_hard": {
+    "value": 72240,
+    "player": "Martyr224"
   },
-  "score_babylon_easy": {
-    "value": 81400,
-    "player": "Leg0Lars"
+  "score_memphis_hard": {
+    "value": 104250,
+    "player": "blumath"
+  },
+  "score_florentia_normal": {
+    "value": 89540,
+    "player": "maddog1334"
+  },
+  "kps_thebes_easy": {
+    "value": 10,
+    "player": "Vol_Jbolaz"
+  },
+  "score_mithila_normal": {
+    "value": 117130,
+    "player": "BloodMoon54"
+  },
+  "score_corinth_normal": {
+    "value": 184910,
+    "player": "Moog94"
+  },
+  "score_corinth_hard": {
+    "value": 183858,
+    "player": "ballzac"
+  },
+  "score_mithila_easy": {
+    "value": 122856,
+    "player": "hustler"
+  },
+  "score_thebes_easy": {
+    "value": 124430,
+    "player": "Dragonling98"
+  },
+  "score_memphis_normal": {
+    "value": 63320,
+    "player": "blumath"
+  },
+  "score_florentia_easy": {
+    "value": 90098,
+    "player": "Dragonling98"
+  },
+  "score_cairo_hard": {
+    "value": 72576,
+    "player": "ImperialUser"
+  },
+  "score_thebes_hard": {
+    "value": 91670,
+    "player": "Martyr224"
+  },
+  "score_babylon_hard": {
+    "value": 69494,
+    "player": "ballzac"
+  },
+  "score_florentia_hard": {
+    "value": 104166,
+    "player": "ballzac"
   },
   "kps_rome_normal": {
     "value": 12,
@@ -209,80 +269,20 @@ try{window.FOLDWARS_KONG_HISTATS={
     "value": 9,
     "player": "BloodMoon54"
   },
-  "kps_memphis_normal": {
-    "value": 10,
-    "player": "Znarf"
-  },
-  "kps_rome_easy": {
-    "value": 11,
-    "player": "BrianCovington"
-  },
-  "score_rome_normal": {
-    "value": 369640,
-    "player": "Moog94"
-  },
-  "kps_thebes_easy": {
-    "value": 10,
-    "player": "Vol_Jbolaz"
-  },
-  "kps_mithila_normal": {
-    "value": 10,
-    "player": "bittramp"
-  },
-  "score_mithila_easy": {
-    "value": 122856,
-    "player": "hustler"
-  },
-  "score_thebes_normal": {
-    "value": 121290,
-    "player": "Ticwith"
+  "score_cairo_easy": {
+    "value": 122808,
+    "player": "Boyinawell"
   },
   "score_rome_easy": {
     "value": 370920,
     "player": "banjomaster"
   },
-  "score_memphis_hard": {
-    "value": 104250,
-    "player": "blumath"
-  },
-  "score_corinth_normal": {
-    "value": 184910,
-    "player": "Moog94"
-  },
-  "score_memphis_easy": {
-    "value": 53568,
-    "player": "mrbb62964"
-  },
   "score_cairo_normal": {
     "value": 117100,
     "player": "Moog94"
   },
-  "score_thebes_hard": {
-    "value": 91670,
-    "player": "Martyr224"
-  },
-  "score_babylon_normal": {
-    "value": 77630,
-    "player": "olinn"
-  },
-  "score_florentia_normal": {
-    "value": 89540,
-    "player": "maddog1334"
-  },
-  "score_florentia_hard": {
-    "value": 104166,
-    "player": "ballzac"
-  },
-  "score_florentia_easy": {
-    "value": 89980,
-    "player": "Zomboconjon"
-  },
-  "score_mithila_normal": {
-    "value": 117130,
-    "player": "BloodMoon54"
-  },
-  "score_thebes_easy": {
-    "value": 124360,
-    "player": "fiercepaw"
+  "score_rome_normal": {
+    "value": 369640,
+    "player": "Moog94"
   }
 };}catch(e){}
